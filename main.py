4a88ea18e1f99ec6fa84649ac07bcb6f3ddf8adc
SSFoxrr <zainwalker97@gmail.com>
2025-07-10 19:23:27 -0500

    Add working matplotlib plot

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-print("hello")
+import matplotlib.pyplot as plt
+import numpy as np
 
+x = [1, 2, 2.5, 3, 4]
+y = [1, 4, 7, 9, 15]
+plt.plot(x, y, 'ro')
+plt.axis([0, 6, 0, 20])
 
-
-#https://github.com/SSFoxrr/001LinearRegression.git
+plt.show()
